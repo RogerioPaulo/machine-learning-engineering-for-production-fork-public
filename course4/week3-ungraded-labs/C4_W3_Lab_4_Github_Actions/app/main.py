fa0f06b190c99a6696a7cd28bb27be10d41bb331
R.Paulo <rogerio.a.paulo@gmail.com>
2023-07-06 15:34:24 +0100

    Code refactoring: Added comments; Testing the CI/CD pipeline.

--- course4/week3-ungraded-labs/C4_W3_Lab_4_Github_Actions/app/main.py
+++ course4/week3-ungraded-labs/C4_W3_Lab_4_Github_Actions/app/main.py
@@ -5,7 +5,7 @@ from fastapi import FastAPI
 from pydantic import BaseModel, conlist
 
 
-
+# Create FastAPI app
 app = FastAPI(title="Predicting Wine Class with batching")
 
 # Open classifier in global scope
@@ -14,12 +14,20 @@ with open("models/wine.pkl", "rb") as file:
 
 
 class Wine(BaseModel):
+    """
+    Data model for request body with a batch of wines.
+    """
     batches: List[conlist(item_type=float, min_items=13, max_items=13)]
 
 
 @app.post("/predict")
 def predict(wine: Wine):
+    """
+    Predict wine class.
+    :param wine: the wine data to predict
+    :return: the type of wine predicted
+    """
     batches = wine.batches
     np_batches = np.array(batches)
-    pred = clf.predict(np_batches).tolist()
-    return {"Prediction": pred}
+    prediction = clf.predict(np_batches).tolist()
+    return {"Prediction": prediction}
